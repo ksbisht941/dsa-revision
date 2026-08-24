ca444b9246d4080c4a919a94171de3118a766531
Kevin <ksbisht941@gmail.com>
2026-08-24 23:20:35 +0530

    feat: implement iterative logic to merge two sorted linked lists

--- linked-list/merge_two_sorted_lists.py
+++ linked-list/merge_two_sorted_lists.py
@@ -7,7 +7,24 @@ class ListNode:
 
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        raise NotImplementedError("Implement your solution here")
+        dummy = ListNode(-1)
+        merge = dummy
+
+        while list1 and list2:
+            if list1.val <= list2.val:
+                merge.next = list1
+                list1 = list1.next
+            else:
+                merge.next = list2
+                list2 = list2.next
+
+            merge = merge.next
+
+        merge.next = list1 if list1 else list2
+
+        return dummy.next
+
+
 
 if __name__ == "__main__":
     def create_linked_list(arr):
